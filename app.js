8b4018ed8a95e43eb3aca5c815cf0dea9d9c62ae
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2018-10-03 17:23:54 -0400

    Attempting to add Objective6 for Lab

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-let name = "Kyle Phillips";
+let name = Kyle;
 
 const usstates = '50';
 
 var sum = add(5 , 4);
+
+if (name >= '4D')  {
+alert('Back of the line chump!');
+} else  {
+    alert('Next!')
+}
