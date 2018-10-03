8ce972b94a5df28d0ae746f4ad08ec31b5955900
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2018-10-03 17:34:46 -0400

    Changed if statement to target first letter of name and not whole name. Also added comments and sayHello Function

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-let name = Kyle;
+let name = [Kyle];
 
 const usstates = '50';
 
 var sum = add(5 , 4);
 
-if (name >= '4D')  {
+// Added if else statment sot that if name stats 
+// with letter after L and alert of Back of the line is displayed
+if (name [0] >= '4D')  {
 alert('Back of the line chump!');
 } else  {
     alert('Next!')
+}
+
+function sayHello () {
+    alert ('Hello World!');
 }
