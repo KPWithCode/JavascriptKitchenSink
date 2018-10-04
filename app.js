9a3988dfd6a589fd25e005a549ece04172f84632
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2018-10-04 16:09:29 -0400

    new updates

--- app.js
+++ app.js
@@ -1,32 +1,54 @@
-let name = [Kyle];
+let name = 'Zed';
 
 const usstates = '50';
 
-var sum = add(5 , 4);
+var sum = 5 + 4;
+console.log(sum);
+
 
 // Added if else statment sot that if name stats 
 // with letter after L and alert of Back of the line is displayed
-if (name [0] >= charCodeAt(77)) {
-alert('Back of the line chump!');
-} else  {
+if (name.charCodeAt(0) >= 77) {
+    alert('Back of the line chump!');
+} else {
     alert('Next!');
 }
 // sayHello function
-function sayHello () {
-    alert ('Hello World!');
+function sayHello(Phone) {
+    alert(Phone);
 }
-sayHello();
+sayHello('Samsung');
+sayHello('Apple');
+
+function add(water, juice) {
+    (water + juice)
+    return name
+}
+let myNum = add(21, 19);
+console.log(myNum);
+
 
 //checkAge function[]
 
-let name = ['Charles','Abby', 'James', 'John']   ;
-function checkAge (name,age)    
-if (age >21){
-    alert ('Sorry ${name} you aren\'t old enough to view this page!');
-} else (age>21)
-    alert ('Come on in Budster');
-    
-checkAge([0],21);
-checkAge([1],27);
-checkAge([2],18);
-checkAge ([3],17);
+let names = ['Charles', 'Abby', 'James', 'John'];
+
+function checkAge(name, age) {
+    if (age < 21) {
+        alert(`Sorry ${name} you aren't old enough to view this page!`);
+    } else {
+        alert('Come on in Budster');
+    }
+}
+
+checkAge(names[0], 21);
+checkAge(names[1], 27);
+checkAge(names[2], 18);
+checkAge(names[3], 17);
+
+// Array for favorite Vegetables
+var favVeggies = ['Carrots', 'Radish', 'Cabbage', 'Lettuce'];
+
+for(let i = 0; i < favVeggies.length; i++)   {
+    console.log (favVeggies[i]);
+
+}
